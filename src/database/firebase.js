31dcf819505e1c2409410f7e69696356bca96c1a
CHINMAY  GAIKAR <team.codegen@gmail.com>
2025-11-02 17:56:02 +0530

    Firebase env removed

--- src/database/firebase.js
+++ src/database/firebase.js
@@ -5,13 +5,7 @@ import { getStorage } from 'firebase/storage';
 
 // --- Firebase Configuration (REPLACE WITH YOUR ACTUAL KEYS) ---
 const firebaseConfig = {
-  apiKey: "AIzaSyCQ4cCIfLVrsq2g6ywD6hXaN3Ejt-Vs-H0",
-  authDomain: "scrum-93033.firebaseapp.com",
-  projectId: "scrum-93033",
-  storageBucket: "scrum-93033.firebasestorage.app",
-  messagingSenderId: "370355233221",
-  appId: "1:370355233221:web:9ce250e19d28c2eecd2bac",
-  measurementId: "G-TH34V54YPL"
+
 };
 
 // Initialize Firebase
